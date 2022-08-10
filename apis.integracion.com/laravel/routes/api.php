6e05c49b24f9ac87cb179049f6719911527840bf
BRYANJASH <108773739+BRYANJASH@users.noreply.github.com>
2022-08-10 09:52:41 -0500

    Update api.php

--- apis.integracion.com/laravel/routes/api.php
+++ apis.integracion.com/laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\v1\ClientesController;
+use App\Http\Controllers\v1\AlumnosController;
 
 /*
 |--------------------------------------------------------------------------
